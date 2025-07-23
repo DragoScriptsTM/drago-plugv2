bbd94d607fe9b6c343cb39f12a33ff2baab63f9e
Marlo <gutjahrmarlon@gmail.com>
2025-07-23 20:38:38 +0200

    ps-dispatch drugsale added

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,12 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-- Functie om politie te waarschuwen met 75% kans via ps-dispatch export
+local function TryDispatchAlert()
+    if math.random() <= 0.75 then
+        exports['ps-dispatch']:DrugSale()
+    end
+end
+
 -- Maak dealer ped en target zone
 CreateThread(function()
     local model = Config.DealerPed.model
@@ -50,10 +57,12 @@ RegisterNUICallback("close", function(_, cb)
     cb("ok")
 end)
 
--- Verkoop items aan server doorgeven
+-- Verkoop items aan server doorgeven + trigger dispatch alert via export
 RegisterNUICallback("sellItems", function(data, cb)
     if data and data.items then
         TriggerServerEvent("drago-plugv2:sellItems", data.items)
+        -- Probeer politie te waarschuwen via ps-dispatch export
+        TryDispatchAlert()
     end
     cb("ok")
 end)
@@ -64,3 +73,24 @@ RegisterNUICallback("requestStats", function(_, cb)
         cb(stats)
     end)
 end)
+
+-- Helper functies om gender en straat te krijgen (indien nog niet aanwezig)
+
+function GetPlayerGender()
+    local ped = PlayerPedId()
+    local model = GetEntityModel(ped)
+    if model == GetHashKey("mp_m_freemode_01") then
+        return "male"
+    elseif model == GetHashKey("mp_f_freemode_01") then
+        return "female"
+    else
+        return "unknown"
+    end
+end
+
+function GetStreetAndZone(coords)
+    local streetName, zone = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
+    local streetLabel = GetStreetNameFromHashKey(streetName)
+    local zoneLabel = GetNameOfZone(coords.x, coords.y, coords.z)
+    return streetLabel .. ", " .. zoneLabel
+end
